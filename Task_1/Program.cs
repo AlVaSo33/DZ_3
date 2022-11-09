e1229f540d85286f0bb807f686d92ded7b2d7826
Alexander Soloviev <asolovev@yandex.ru>
2022-11-09 23:24:14 +0300

    AddGitignore

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int GetNumberPalindrome(int number)
+{
+    int firstMeaning = number;
+    int secondMeaning = 0;
+    while (number > 0)
+    {
+        int number2 = number % 10;
+        secondMeaning = secondMeaning * 10 + number2;
+        number = number / 10;
+
+    }
+
+    if (secondMeaning == firstMeaning)
+        Console.WriteLine("Число является палиндромом");
+    else
+        Console.WriteLine("Число не является палиндромом");
+
+    return (number);
+}
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+
+int Meaning = GetNumberPalindrome(number);
+
+
